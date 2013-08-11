05d16bbce587e15996b7e64715231b267b228fc5
Juan Antonio Plaza <jplazaarguello@gmail.com>
2013-08-11 15:52:49 -0500

    Bump version

--- service/project.clj
+++ service/project.clj
@@ -9,7 +9,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal.service "0.1.10"
+(defproject io.pedestal/pedestal.service "0.1.11-SNAPSHOT"
   :description "Pedestal Service"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  ;; logging
